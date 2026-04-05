63b3a09073a69707d66307afcf91590f25463281
Arya Kumar <arkumar@chapman.edu>
2026-04-05 02:54:59 -0700

    CORS

--- scheduler/api.py
+++ scheduler/api.py
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://carbon-play-jxqaxmt7g-arya-kumar1s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
